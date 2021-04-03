7b3a69f0b8a8be9400e414cbc45cb188dcec1dea
Bayuanang <80206057+NovaBayuAnangIstiawan@users.noreply.github.com>
2021-04-03 16:19:57 +0700

    Add files via upload

---
+++ pertemuan 5/Teori/src/Data/NmBuah.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+/**
+ *
+ * @author nothing
+ */
+public class NmBuah {
+    private String Buah;
+    
+    NmBuah(){
+    this.Buah = "";
+    }
+    public String setBuah(String Buah) {
+       return this.Buah = Buah;
+    }
+
+    public String getBuah() {
+        return Buah;
+    }
+    
+
+}
